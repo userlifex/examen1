8f4f091e90a7e224c260f35af8664b115a00abfb
userlifex <eholguinruiz@gmail.com>
2022-01-26 22:07:17 -0500

    finish work

--- app/src/main/java/com/example/ex1juniorholguin/Activity2.kt
+++ app/src/main/java/com/example/ex1juniorholguin/Activity2.kt
@@ -2,6 +2,9 @@ package com.example.ex1juniorholguin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_2.*
 
 class Activity2 : AppCompatActivity() {
@@ -18,6 +21,30 @@ class Activity2 : AppCompatActivity() {
         val nombre = intent.getStringExtra(NAME)
 
         showName.setText(nombre)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_option_2, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.back -> {
+            goBack()
+            true
+        }
+
+        else -> {
+// If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun goBack() {
+        this.finish()
     }
 
 
--- app/src/main/java/com/example/ex1juniorholguin/MainActivity.kt
+++ app/src/main/java/com/example/ex1juniorholguin/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.ex1juniorholguin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +17,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean
+    {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_option, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.nextView -> {
+            val i= Intent(this,Activity2::class.java)
+            i.putExtra(Activity2.NAME,nombre)
+            i.putExtra(Activity2.EMAIL,email)
+            this.startActivity(i)
+            true
+        }else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun goBack() {
+        this.finish()
     }
 
     fun jugar(view: View){
